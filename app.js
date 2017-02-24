33318dd102aeede865d428e08546e4da08670d77
xlmnxp <xlmnxp@outlook.sa>
2017-02-24 23:44:03 +0300

    .

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ var express = require("express");
 var app = express();
 var api = new hsoub_1.io();
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
 app.get("/", function (req, res) {
     api.community('webdev', null, function (err, tres) {
