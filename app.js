dd6857ce23701c150f59d3c56fcbd3c49a05b465
xlmnxp <xlmnxp@outlook.sa>
2017-02-27 01:32:14 +0300

    .

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 "use strict";
 exports.__esModule = true;
-var hsoub_1 = require("hsoub");
 var express = require("express");
 var app = express();
-var api = new hsoub_1.io();
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', '*');
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
         res.send(200);
@@ -16,8 +14,10 @@ app.use(function (req, res, next) {
         next();
     }
 });
-app.get("/", function (req, res) {
-    api.community('webdev', null, function (err, tres) {
+app.get("/:id", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    console.log("new request : " +req.params['id']);
+    (new (require("hsoub")).io()).community(req.params['id'], null, function (err, tres) {
         if (err) {
             console.log(err);
             return;
@@ -26,10 +26,11 @@ app.get("/", function (req, res) {
             name: tres.name,
             followers: tres.followers,
             image: tres.image,
-            description: tres.description
+            description: tres.description,
+            url: tres.url
         };
         console.log(data);
-        res.send(data);
+        res.send(JSON.stringify(data,null,4));
     });
 });
 app.listen((process.env.PORT || 80), function () {
